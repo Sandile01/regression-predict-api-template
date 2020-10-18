d906f359af5026c62007f4df24b647a6298b1429
Nthabiseng Moela <nthabisengmoela1@gmail.com>
2020-10-18 10:18:40 +0200

    Update request.py

--- utils/request.py
+++ utils/request.py
@@ -21,7 +21,7 @@
 import requests
 import pandas as pd
 import numpy as np
-
+from model import _preprocess_data
 
 # Load data from file to send as an API POST request.
 # We prepare a DataFrame with the public test set + riders data
@@ -29,7 +29,7 @@ import numpy as np
 test = pd.read_csv('data/test_data.csv')
 riders = pd.read_csv('data/riders.csv')
 test = test.merge(riders, how='left', on='Rider Id')
-
+test = _preprocess_data(test)
 
 # Convert our DataFrame to a JSON string.
 # This step is necessary in order to transmit our data via HTTP/S
